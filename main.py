8c9c73353b9fc28ec1f14efb7fc35c989a4de00f
jdszekeres <jdszekeres@gmail.com>
2023-01-11 14:02:21 -0800

    work

--- main.py
+++ main.py
@@ -10,21 +10,26 @@ import random # select home style
 import datetime # for time related tasks
 import refs # constants to refer to
 import os# paths
+import jstyleson
 
 app = Flask("city builder") # create an app object for flask
-
+f=open("sample.json")
+b=jstyleson.loads(f.read())
 assets_folder = "assets"
 home_count = 1 # number of home styles (max 2nd number in assets/home) ie. home2-1.png the 1 is important
 
 @app.route("/")
 def index():
-    money=0
-    grid=[[]]
-    resources={
-            1:4,
-            2:5,
-            3:1}
-    return render_template("index.html",money=money,grid=grid,resources=resources.items()) # render main page 
+    money=b["money"]
+    grid=b["grid"]
+    resources=b["resources"]
+    tpm=b["tax"]
+    return render_template("index.html",money=money,grid=grid,resources=resources.items(),tpm=tpm) # render main page 
+
+
+app.jinja_env.globals.update(round=round) # pass functions jinga
+app.jinja_env.globals.update(rem_num=refs.rem_num) # pass functions jinga
 
 if __name__ == "__main__":
     app.run("0.0.0.0",8080,debug=True) # if run from file, debug mode
+    f.close()
--- refs.py
+++ refs.py
@@ -5,3 +5,6 @@ PRODUCTION_ITEMS=["","Wood","Steel","Plastic","Seeds","Sand","Ore"] # list of pr
 
 def time_in_future(mins) -> float:
     return (datetime.now() + timedelta(minutes=mins)).timestamp() # given x minutes in future, return epoch time for that future time
+def rem_num(string) -> str:
+    return ''.join(i for i in string if not i.isdigit())
+
